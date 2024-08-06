b956f51f31070b90d7abfb98bee0d9cf48c3a799
Aldo Ortega González <aeledeo.9@gmail.com>
2024-08-05 18:04:22 -0600

    ya guarda

--- backend/src/controllers/rsvp.controller.js
+++ backend/src/controllers/rsvp.controller.js
@@ -1,9 +1,29 @@
 import qrcode from "qrcode";
+import Rsvp from '../models/rsvp.model.js';
+
 export const register = async (req, res) =>{
     try{
-        const { nombre, apellidos, email, } = req.body;
-        console.log("estamos a nada de cerrar el dato ");
-        return res.sendStatus(200);
+        const { nombre, apellidos, email, asistencia1, asistencia2 } = req.body;
+
+        // Crear un nuevo documento de RSVP
+        const newRsvp = new Rsvp({
+            nombre,
+            apellidos,
+            email,
+            asistencia1,
+            asistencia2
+        });
+
+        // Generar el código QR y almacenarlo en el documento
+        const qrCodeText = `Nombre: ${nombre}, Apellidos: ${apellidos}, Email: ${email}`;
+        const qrCodeImage = await qrcode.toBuffer(qrCodeText);
+
+        newRsvp.qrcodeImage = qrCodeImage;
+
+        // Guardar el nuevo documento en la base de datos
+        await newRsvp.save();
+
+        res.status(201).json(newRsvp);
         
     }
     catch(error){
