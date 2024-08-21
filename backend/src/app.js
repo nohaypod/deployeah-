6fec98b181fe690daae51cdb15744285b156b8c8
nohaypod <aeledeo.9@gmail.com>
2024-08-21 14:45:07 -0600

    actualizaciones para deployear

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import rsvpRoutes from "./routes/rsvp.routes.js"
-import cors from 'cors'
+import cors from 'cors';
+import path from "path";
+import { fileURLToPath } from 'url';
 
+// Crear __dirname manualmente
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
  app.use(cors({
@@ -12,4 +17,15 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(rsvpRoutes);
+if (true) {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../../', 'client', 'dist', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 export default app;
